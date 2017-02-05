25c12e04aca697d644538520e775b8cd27860272
Drew Ritter <dritter@razelon.com>
2017-02-05 13:42:43 -0500

    Initial Commit. Blank project with window open, stays until the X is pressed.
    Signed-off-by: Drew Ritter <dritter@razelon.com>

---
+++ ChronoLoop/ChronoLoop/ChronoLoop.cpp
@@ -0,0 +1,118 @@
+#include "stdafx.h"
+
+HWND hwnd;
+LPCTSTR WndClassName = L"ChronoWindow";
+HINSTANCE hInst;
+
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void Update();
+
+int APIENTRY wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
+	if (!InitializeWindow(hInstance, nCmdShow, 800, 600, true)) {
+		MessageBox(NULL, L"Kablamo.", L"The window broked.", MB_ICONERROR | MB_OK);
+	}
+
+	// Initialize Renderer / VR
+
+	// Update everything
+	Update();
+
+	// Cleanup
+
+	return 0;
+}
+
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed) {
+#if _DEBUG || CONSOLE_OVERRIDE
+	if (AllocConsole()) {
+		FILE* pCout;
+		freopen_s(&pCout, "CONOUT$", "w", stdout);
+		SetConsoleTitle(L"Chrono::Loop - Debug Console");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+	}
+#endif
+
+	WNDCLASSEX wc;
+
+	HINSTANCE hDll;
+	hDll = LoadLibrary(L"SHELL32.dll");
+
+	wc.cbSize = sizeof(WNDCLASSEX);    //Size of our windows class
+	wc.style = (CS_HREDRAW | CS_VREDRAW);    //class styles
+	wc.lpfnWndProc = WndProc;    //Default windows procedure function
+	wc.cbClsExtra = NULL;    //Extra bytes after our wc structure
+	wc.cbWndExtra = NULL;    //Extra bytes after our windows instance
+	wc.hInstance = hInstance;    //Instance to current application
+	wc.hIcon = LoadIcon(hDll, MAKEINTRESOURCE(512));    //Title bar Icon
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);    //Default mouse Icon
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);    //Window bg color
+	wc.lpszMenuName = NULL;    //Name of the menu attached to our window
+	wc.lpszClassName = WndClassName;    //Name of our windows class
+	wc.hIconSm = LoadIcon(hDll, MAKEINTRESOURCE(512)); //Icon in your taskbar
+
+	if (!RegisterClassEx(&wc))    //Register our windows class
+	{
+		//if registration failed, display error
+		DWORD code = GetLastError();
+		MessageBox(NULL, L"Error registering class", L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	hwnd = CreateWindowEx(                                     //Create our Extended Window
+												NULL,                                //Extended style
+												WndClassName,                        //Name of our windows class
+												L"Chrono::Loop",                     //Name in the title bar of our window
+												WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME, //style of our window
+												600, 150,										         //Top left corner of window
+												width,                               //Width of our window
+												height,                              //Height of our window
+												NULL,                                //Handle to parent window
+												NULL,                                //Handle to a Menu
+												hInstance,                           //Specifies instance of current program
+												NULL                                 //used for an MDI client window
+	);
+
+	if (!hwnd) {
+		MessageBox(NULL, L"Error creating window", L"Error", MB_OK | MB_ICONERROR);
+		return 1;
+	}
+
+	ShowWindow(hwnd, ShowWnd);
+	UpdateWindow(hwnd);
+
+	return true;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+void Update() {
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+	while (true) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			// Handle windows message.
+			if (msg.message == WM_QUIT) {
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+			// Input.Update();
+			// Logic.Update();
+			// Renderer.Render();
+		}
+	}
+}
+
+
+
