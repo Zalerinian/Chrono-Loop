f73dc2a1becbc210e7244474ea9901d830e1ac03
Raymondckd <rl.lewski0@gmail.com>
2017-02-07 10:53:39 -0500

    Sound wrapper skeletal-ized

--- ChronoLoop/SoundEngine/SoundEngine.cpp
+++ ChronoLoop/SoundEngine/SoundEngine.cpp
@@ -4,4 +4,89 @@
 #include "stdafx.h"
 #include "SoundEngine.h"
 
+using namespace AK;
 
+AudioWrapper * AudioWrapper::audioSystem = nullptr;
+
+AudioWrapper::AudioWrapper()
+{
+
+}
+
+AudioWrapper::~AudioWrapper()
+{
+
+}
+//-------Initialize-Shutdown-Update------------------------------------
+bool AudioWrapper::initialize()
+{
+
+}
+
+void AudioWrapper::shutdown()
+{
+
+}
+
+void AudioWrapper::update()
+{
+
+}
+//----------------------------------------------------
+void AudioWrapper::setWorldScale(float _scale)
+{
+
+}
+
+//------------Add/Remove-Listeners/Emitters-----------------------------------------
+bool AudioWrapper::addListener(const Listener* _listener, const char * _name)
+{
+
+}
+
+bool AudioWrapper::removeListener(const Listener* _listener)
+{
+
+}
+
+bool AudioWrapper::addEmitter(const Emitter * _emitter, const char * _name)
+{
+
+}
+
+bool AudioWrapper::removeEmitter(const Emitter * _emitter)
+{
+
+}
+
+//-------------EVENTS-----------------------------------------------
+bool AudioWrapper::makeEvent(AudioEvent _id, float * _pos)
+{
+
+}
+
+bool AudioWrapper::makeEvent(AudioEvent _id, const Emitter * _emitter)
+{
+
+}
+
+bool AudioWrapper::makeEvent(AudioEvent _id, unsigned int _listenerID)
+{
+
+}
+//------------Soundbank-Stuff---------------------------------------------
+
+void AudioWrapper::setBasePath(const wchar_t* _strPath)
+{
+
+}
+
+bool AudioWrapper::loadSoundBank(const wchar_t* _BankName)
+{
+
+}
+
+bool AudioWrapper::unloadSoundBank(const wchar_t* _BankName)
+{
+
+}
--- ChronoLoop/SoundEngine/SoundEngine.h
+++ ChronoLoop/SoundEngine/SoundEngine.h
@@ -1,29 +1,55 @@
 #pragma once
+//Credit to John Murphy
+#include <vector>
+#include <map>
 
 #include "../ObjectEngine/Components.h"
-#include <vector>
+#include "AkSoundEngineDLL.h"
 
-std::vector<Emitter> emitters;
-std::vector<Listener> listeners;
+#define DLL __declspec(dllexport)
 
+//Needed to work with Wwise soundbanks and the .h they generate
+#if defined(_WIN64)
+typedef unsigned __int64 AudioEvent;				///< Integer (unsigned) type for pointers
+#else
+typedef __w64 unsigned int AudioEvent;			///< Integer (unsigned) type for pointers
+#endif
 
+class AudioWrapper
+{
+	static AudioWrapper* audioSystem;
 
+private:
+	std::vector<Emitter> emitters;
+	std::vector<Listener> listeners;
+	std::map<std::wstring, AkBankID> RegisteredSoundBanks;
 
+	bool isInitialize = false;
+	float worldScale;
+public:
+	DLL AudioWrapper();
+	DLL ~AudioWrapper();
 
+	DLL bool initialize();
+	DLL void shutdown();
+	DLL void update();
 
+	DLL void setWorldScale(float _scale);
+	DLL bool isInitialized() { return isInitialize; }
 
+	DLL bool addListener(const Listener * _listener, const char* _name);
+	DLL bool removeListener(const Listener * _listener);
+	DLL bool addEmitter(const Emitter * _emitter, const char* _name);
+	DLL bool removeEmitter(const Emitter * _emitter);
 
+	//Posts an event at a pos, emitter location, or near a listener.
+	DLL bool makeEvent(AudioEvent _id, float* _pos);
+	DLL bool makeEvent(AudioEvent _id, const Emitter * _emitter);
+	DLL bool makeEvent(AudioEvent _id, unsigned int _listenerID = 0);
 
+	//Set soundbank path(s)
+	DLL void setBasePath(const wchar_t* _strPath);
+	DLL bool loadSoundBank(const wchar_t* _BankName);
+	DLL bool unloadSoundBank(const wchar_t* _BankName);
 
-
-
-
-
-
-
-
-
-
-
-
-
+};
