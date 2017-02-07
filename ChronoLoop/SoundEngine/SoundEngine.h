f438350122f50f9223da24cfd250f3f15c058288
Raymondckd <rl.lewski0@gmail.com>
2017-02-07 09:02:53 -0500

    Sound needs added, engine initialized. Components started

---
+++ ChronoLoop/SoundEngine/SoundEngine.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#pragma comment(lib, "ObjectEngine.lib")
+
+#include "../ObjectEngine/Components.h"
+#include <vector>
+
+std::vector<Emitter> emitters;
+std::vector<Listener> listeners;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
