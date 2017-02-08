e9eeadcec354300c6672c0ce75f3a116e9fae1c9
Chris Burt <Cwburt@fullsail.edu>
2017-02-08 17:55:16 -0500

    Changed perams of functions to match naming convention

--- ChronoLoop/ChronoLoop/Physics.cpp
+++ ChronoLoop/ChronoLoop/Physics.cpp
@@ -8,50 +8,50 @@
 
 
 
-bool Physics::SameSign(float _f1, float _f2)
+bool SameSign(float _f1, float _f2)
 {
 	if ((_f1 > 0 && _f2 > 0) || (_f1 < 0 && _f2 < 0))
 		return true;
 	return false;
 }
 
-bool Physics::IntersectRayToTriangle(vec4f& vert0, vec4f& vert1, vec4f& vert2, vec4f& norm, vec4f& start, vec4f& d, float& t)
+bool Physics::IntersectRayToTriangle(vec4f& _vert0, vec4f& _vert1, vec4f& _vert2, vec4f& _normal, vec4f& _start, vec4f& _dir, float& _time)
 {
-	if (((norm * d) > 0 || (vert0 - start) * norm) > 0)
+	if (((_normal * _dir) > 0 || (_vert0 - _start) * _normal) > 0)
 		return false;
 
-	vec4f sa = vert0 - start;
-	vec4f sb = vert1 - start;
-	vec4f sc = vert2 - start;
+	vec4f sa = _vert0 - _start;
+	vec4f sb = _vert1 - _start;
+	vec4f sc = _vert2 - _start;
 	vec4f N1, N2, N3;
 	N1 = sc ^ sb;
 	N2 = sa ^ sc;
 	N3 = sb ^ sa;
-	float dn1 = d * N1;
-	float dn2 = d * N2;
-	float dn3 = d * N3;
+	float dn1 = _dir * N1;
+	float dn2 = _dir * N2;
+	float dn3 = _dir * N3;
 
 	if (dn1 == 0.0f && dn2 == 0.0f && dn3 == 0.0f)
 	{
-		t = 0;
+		_time = 0;
 		return false;
 	}
 
 	if (SameSign(dn1, dn2) && SameSign(dn2, dn3))
 	{
-		float offset = norm * vert0;
-		t = ((offset - (start * norm)) / (norm * d));
+		float offset = _normal * _vert0;
+		_time = ((offset - (_start * _normal)) / (_normal * _dir));
 		return true;
 	}
 }
 
-bool Physics::IntersectRayToSphere(vec4f& p, vec4f& d, vec4f& center, float radius, float& t, vec4f& q)
+bool Physics::IntersectRayToSphere(vec4f& _pos, vec4f& _dir, vec4f& _center, float _radius, float& _time, vec4f& _out)
 {
-	vec4f check = p - center;
+	vec4f check = _pos - _center;
 
-	vec4f m = p - center;
-	float b = m * d;
-	float c = (m * m) - (radius * radius);
+	vec4f m = _pos - _center;
+	float b = m * _dir;
+	float c = (m * m) - (_radius * _radius);
 	if (c < 0.0f || b > 0.0f)
 		return false;
 
@@ -59,26 +59,26 @@ bool Physics::IntersectRayToSphere(vec4f& p, vec4f& d, vec4f& center, float radi
 	if (dis < 0.0f)
 		return false;
 
-	t = -b - sqrt(dis);
-	if (t < 0.0f)
+	_time = -b - sqrt(dis);
+	if (_time < 0.0f)
 	{
-		t = 0.0f;
-		vec4f np = { p.data.x + t, p.data.y + t, p.data.z + t, 1.0f };
-		q = np ^ d;
+		_time = 0.0f;
+		vec4f np = { _pos.data.x + _time, _pos.data.y + _time, _pos.data.z + _time, 1.0f };
+		_out = np ^ _dir;
 	}
 
 	return true;
 }
 
-bool Physics::IntersectRayToCylinder(vec4f & sa, vec4f & n, vec4f & p, vec4f & q, float r, float & t)
+bool Physics::IntersectRayToCylinder(vec4f& _start, vec4f& _normal, vec4f& _point1, vec4f& _point2, float _radius, float& _time)
 {
-	vec4f d = q - p;
-	vec4f od = p - q;
-	vec4f m = sa - p; 
+	vec4f d = _point2 - _point1;
+	vec4f od = _point1 - _point2;
+	vec4f m = _start - _point1; 
 
 	float dd = d * d;
-	float nd = n * d;
-	float mn = m * n;
+	float nd = _normal * d;
+	float mn = m * _normal;
 	float md = m * d;
 	float mm = m * m;
 
@@ -86,8 +86,8 @@ bool Physics::IntersectRayToCylinder(vec4f & sa, vec4f & n, vec4f & p, vec4f & q
 		return false;
 
 	float a = dd - nd * nd;
-	float b = dd*mn - nd*md;
-	float c = dd*(mm - r*r) - md*md;
+	float b = dd * mn - nd * md;
+	float c = dd * (mm - _radius * _radius) - md * md;
 
 	if (abs(a) < FLT_EPSILON)
 		return false;
@@ -96,96 +96,96 @@ bool Physics::IntersectRayToCylinder(vec4f & sa, vec4f & n, vec4f & p, vec4f & q
 	if (dis < 0)
 		return false;
 
-	t = (-b - sqrt(dis)) / a;
+	_time = (-b - sqrt(dis)) / a;
 
-	if (t < 0)
+	if (_time < 0)
 		return false;
 
-	vec4f col = sa + n * t;
+	vec4f col = _start + _normal * _time;
 
-	if (od * (q - col) > 0 || d * (p - col) > 0)
+	if (od * (_point2 - col) > 0 || d * (_point1 - col) > 0)
 		return false;
 
 	return true;
 }
 
-bool Physics::IntersectRayToCapsule(vec4f & sa, vec4f & n, vec4f & p, vec4f & q, float r, float & t)
+bool Physics::IntersectRayToCapsule(vec4f& _start, vec4f& _normal, vec4f& _point1, vec4f& _point2, float _radius, float& _time)
 {
 	float fTime = FLT_MAX;
-	t = FLT_MAX;
+	_time = FLT_MAX;
 	bool bReturn = false;
 
-	if (IntersectRayToCylinder(sa, n, p, q, r, t))
+	if (IntersectRayToCylinder(_start, _normal, _point1, _point2, _radius, _time))
 	{
-		fTime = min(t, fTime);
-		t = fTime;
+		fTime = min(_time, fTime);
+		_time = fTime;
 		bReturn = true;
 	}
 
 	vec4f pcol, qcol;
-	if (IntersectRayToSphere(sa, n, p, r, t, pcol))
+	if (IntersectRayToSphere(_start, _normal, _point1, _radius, _time, pcol))
 	{
-		fTime = min(t, fTime);
-		t = fTime;
+		fTime = min(_time, fTime);
+		_time = fTime;
 		bReturn = true;
 	}
 
-	if (IntersectRayToSphere(sa, n, q, r, t, qcol))
+	if (IntersectRayToSphere(_start, _normal, _point2, _radius, _time, qcol))
 	{
-		fTime = min(t, fTime);
-		t = fTime;
+		fTime = min(_time, fTime);
+		_time = fTime;
 		bReturn = true;
 	}
 	return bReturn;
 }
 
-bool Physics::IntersectMovingSphereToTriangle(vec4f & vert0, vec4f & vert1, vec4f & vert2, vec4f & norm, vec4f & start, vec4f & d, float r, float & t, vec4f & outNormal)
+bool Physics::IntersectMovingSphereToTriangle(vec4f& _vert0, vec4f& _vert1, vec4f& _vert2, vec4f& _normal, vec4f& _start, vec4f& _dir, float _radius, float& _time, vec4f& _outNormal)
 {
 	bool bReturn = false;
 	float fTime = FLT_MAX;
-	t = FLT_MAX;
+	_time = FLT_MAX;
 
-	vec4f verts[3] = { vert0, vert1, vert2 };
-	vec4f offset0 = vert0 + (norm * r);
-	vec4f offset1 = vert1 + (norm * r);
-	vec4f offset2 = vert2 + (norm * r);
+	vec4f verts[3] = { _vert0, _vert1, _vert2 };
+	vec4f offset0 = _vert0 + (_normal * _radius);
+	vec4f offset1 = _vert1 + (_normal * _radius);
+	vec4f offset2 = _vert2 + (_normal * _radius);
 
-	if (IntersectRayToTriangle(offset0, offset1, offset2, norm, start, d, t))
+	if (IntersectRayToTriangle(offset0, offset1, offset2, _normal, _start, _dir, _time))
 	{
-		fTime = min(t, fTime);
-		t = fTime;
-		outNormal = norm;
+		fTime = min(_time, fTime);
+		_time = fTime;
+		_outNormal = _normal;
 		bReturn = true;
 	}
 	else
 	{
-		if (IntersectRayToCapsule(start, d, vert0, vert1, r, t))
+		if (IntersectRayToCapsule(_start, _dir, _vert0, _vert1, _radius, _time))
 		{
-			fTime = min(t, fTime);
-			t = fTime;
-			vec4f temp1 = d * t + start;
-			vec4f temp2 = (vert1 - vert0) * t + vert0;
-			outNormal = (temp1 - temp2).Normalize();
+			fTime = min(_time, fTime);
+			_time = fTime;
+			vec4f temp1 = _dir * _time + _start;
+			vec4f temp2 = (_vert1 - _vert0) * _time + _vert0;
+			_outNormal = (temp1 - temp2).Normalize();
 			bReturn = true;
 		}
 
-		if (IntersectRayToCapsule(start, d, vert1, vert2, r, t))
+		if (IntersectRayToCapsule(_start, _dir, _vert1, _vert2, _radius, _time))
 		{
-			fTime = min(t, fTime);
-			t = fTime;
-			vec4f temp1 = d * t + start;
-			vec4f temp2 = (vert1 - vert0) * t + vert0;
-			outNormal = (temp1 - temp2).Normalize();
+			fTime = min(_time, fTime);
+			_time = fTime;
+			vec4f temp1 = _dir * _time + _start;
+			vec4f temp2 = (_vert1 - _vert0) * _time + _vert0;
+			_outNormal = (temp1 - temp2).Normalize();
 			bReturn = true;
 		}
 
-		if (IntersectRayToCapsule(start, d, vert2, vert0, r, t))
+		if (IntersectRayToCapsule(_start, _dir, _vert2, _vert0, _radius, _time))
 		{
-			fTime = min(t, fTime);
-			t = fTime;
-			vec4f temp1 = d * t + start;
-			vec4f temp2 = (vert1 - vert0) * t + vert0;
-			outNormal = (temp1 - temp2).Normalize();
+			fTime = min(_time, fTime);
+			_time = fTime;
+			vec4f temp1 = _dir * _time + _start;
+			vec4f temp2 = (_vert1 - _vert0) * _time + _vert0;
+			_outNormal = (temp1 - temp2).Normalize();
 			bReturn = true;
 		}
 	}
@@ -193,10 +193,10 @@ bool Physics::IntersectMovingSphereToTriangle(vec4f & vert0, vec4f & vert1, vec4
 	return bReturn;
 }
 
-//bool Physics::IntersectMovingSphereToMesh(vec4f & start, vec4f & d, float r, ED2Mesh * mesh, float & t, vec4f & outNormal)
+//bool Physics::IntersectMovingSphereToMesh(vec4f& _start, vec4f& _dir, float _radius, ED2Mesh* _mesh, float& _time, vec4f& _outNormal)
 //{
 //	bool bCollision = false;
-//	t = FLT_MAX;
+//	_time = FLT_MAX;
 //	float fTime = FLT_MAX;
 //	
 //	unsigned int tempSortedIndicies[3] = { 0,0,0 };
@@ -226,14 +226,72 @@ bool Physics::IntersectMovingSphereToTriangle(vec4f & vert0, vec4f & vert1, vec4
 //			mesh->m_Vertices[tempSortedIndicies[0]].pos,
 //			mesh->m_Vertices[tempSortedIndicies[1]].pos,
 //			mesh->m_Vertices[tempSortedIndicies[2]].pos,
-//			currNorm, start, d, r, fTime, outNormal))
+//			currNorm, _start, _dir, _radius, fTime, _outNormal))
 //		{
-//			t = fminf(t, fTime);
-//			outNormal = currNorm;
+//			_time = fminf(_time, fTime);
+//			_outNormal = currNorm;
 //			bCollision = true;
 //		}
 //	}
 //
 //	return bCollision;
+//}
+
+
+void Physics::BuildPlane(Plane& _plane, vec4f _pointA, vec4f& _pointB, vec4f& _pointC)
+{
+	_plane.mNormal = (_pointA - _pointC) ^ (_pointB - _pointA);
+	_plane.mNormal.Normalize();
+	_plane.mOffset = _pointA * _plane.mNormal;
+}
+
+//Returns 1 if on or in front of plane
+//Returns 2 if behind plane
+int Physics::ClassifyPointToPlane(Plane& _plane, vec4f& _point)
+{
+	float pOffset = _point * _plane.mNormal;
+	if (pOffset >= _plane.mOffset)
+		return 1;
+	return 2;
+}
+
+//Returns 1 if in front of plane
+//Returns 2 if behind plane
+//Returns 3 if intersecting plane
+int Physics::ClassifySphereToPlane(Plane& _plane, Sphere& _sphere)
+{
+	float pOffset = _sphere.mPosition * _plane.mNormal;
+	if (pOffset - _plane.mOffset > _sphere.mRadius)
+		return 1;
+	else if (pOffset - _plane.mOffset < -_sphere.mRadius)
+		return 2;
+	return 3;
+}
+
+//void Physics::BuildFrustum(Frustum& _frustum, float _fov, float _nearDist, float _farDist, float _ratio, matrix4& _camXform)
+//{
+//	vec4f fc = _camXform.axis_pos - _camXform.axis_z * _farDist;
+//	vec4f nc = _camXform.axis_pos - _camXform.axis_z * _nearDist;
+//	float Hnear = 2 * tan(_fov / 2) * _nearDist;
+//	float Hfar = 2 * tan(_fov / 2) * _farDist;
+//	float Wnear = Hnear * _ratio;
+//	float Wfar = Hfar * _ratio;
+//
+//	_frustum.mPoints[0] = fc + _camXform.axis_y * (Hfar * 0.5f) - _camXform.axis_x * (Wfar * 0.5f);
+//	_frustum.mPoints[3] = fc + _camXform.axis_y * (Hfar * 0.5f) + _camXform.axis_x * (Wfar * 0.5f);
+//	_frustum.mPoints[1] = fc - _camXform.axis_y * (Hfar * 0.5f) - _camXform.axis_x * (Wfar * 0.5f);
+//	_frustum.mPoints[2] = fc - _camXform.axis_y * (Hfar * 0.5f) + _camXform.axis_x * (Wfar * 0.5f);
+//			   
+//	_frustum.mPoints[4] = nc + _camXform.axis_y * (Hnear * 0.5f) - _camXform.axis_x * (Wnear * 0.5f);
+//	_frustum.mPoints[5] = nc + _camXform.axis_y * (Hnear * 0.5f) + _camXform.axis_x * (Wnear * 0.5f);
+//	_frustum.mPoints[7] = nc - _camXform.axis_y * (Hnear * 0.5f) - _camXform.axis_x * (Wnear * 0.5f);
+//	_frustum.mPoints[6] = nc - _camXform.axis_y * (Hnear * 0.5f) + _camXform.axis_x * (Wnear * 0.5f);
+//
+//	BuildPlane(_frustum.mFaces[0], _frustum.mPoints[6], _frustum.mPoints[7], _frustum.mPoints[4]);
+//	BuildPlane(_frustum.mFaces[1], _frustum.mPoints[1], _frustum.mPoints[2], _frustum.mPoints[3]);
+//	BuildPlane(_frustum.mFaces[2], _frustum.mPoints[7], _frustum.mPoints[1], _frustum.mPoints[0]);
+//	BuildPlane(_frustum.mFaces[3], _frustum.mPoints[2], _frustum.mPoints[6], _frustum.mPoints[5]);
+//	BuildPlane(_frustum.mFaces[4], _frustum.mPoints[5], _frustum.mPoints[4], _frustum.mPoints[0]);
+//	BuildPlane(_frustum.mFaces[5], _frustum.mPoints[7], _frustum.mPoints[6], _frustum.mPoints[2]);
 //}
 
--- ChronoLoop/ChronoLoop/Physics.h
+++ ChronoLoop/ChronoLoop/Physics.h
@@ -6,16 +6,45 @@
 
 #include "..\ChronoLoop\Common\Math.h"
 
+struct Plane
+{
+	vec4f mNormal;
+	float mOffset;
+};
+
+struct Sphere
+{
+	vec4f mPosition;
+	float mRadius;
+};
+
+struct Frustum
+{
+	Plane* mFaces[6];
+	vec4f* mPoints[8];
+};
+
+struct AABB
+{
+	vec4f mMin;
+	vec4f mMax;
+};
+
 class Physics
 {
 	vec4f mGravity;
 	float mFrictionCo;
 
-	bool SameSign(float _f1, float _f2);
-	bool IntersectRayToTriangle(vec4f& vert0, vec4f& vert1, vec4f& vert2, vec4f& norm, vec4f& start, vec4f& d, float& t);
-	bool IntersectRayToSphere(vec4f &p, vec4f &d, vec4f &center, float radius, float &t, vec4f &q);
-	bool IntersectRayToCylinder(vec4f& sa, vec4f& n, vec4f& p, vec4f& q, float r, float& t);
-	bool IntersectRayToCapsule(vec4f& sa, vec4f& n, vec4f& p, vec4f& q, float r, float& t);
-	bool IntersectMovingSphereToTriangle(vec4f& vert0, vec4f& vert1, vec4f& vert2, vec4f& norm, vec4f& start, vec4f& d, float r, float& t, vec4f& outNormal);
-	//bool IntersectMovingSphereToMesh(vec4f& start, vec4f& d, float r, ED2Mesh* mesh, float& t, vec4f& outNormal);
+	bool IntersectRayToTriangle(vec4f& _vert0, vec4f& _vert1, vec4f& _vert2, vec4f& _normal, vec4f& _start, vec4f& _dir, float& _time);
+	bool IntersectRayToSphere(vec4f& _pos, vec4f& _dir, vec4f& _center, float _radius, float& _time, vec4f& _out);
+	bool IntersectRayToCylinder(vec4f& _start, vec4f& _normal, vec4f& _point1, vec4f& _point2, float _radius, float& _time);
+	bool IntersectRayToCapsule(vec4f& _start, vec4f& _normal, vec4f& _point1, vec4f& _point2, float _radius, float& _time);
+	bool IntersectMovingSphereToTriangle(vec4f& _vert0, vec4f& _vert1, vec4f& _vert2, vec4f& _normal, vec4f& _start, vec4f& _dir, float _radius, float& _time, vec4f& _outNormal);
+	//bool IntersectMovingSphereToMesh(vec4f& _start, vec4f& _dir, float _radius, ED2Mesh* mesh, float& _time, vec4f& _outNormal);
+
+	void BuildPlane(Plane& _plane, vec4f _pointA, vec4f& _pointB, vec4f& _pointC);
+	int ClassifyPointToPlane(Plane& _plane, vec4f& _point);
+	int ClassifySphereToPlane(Plane& _plane, Sphere& _sphere);
+	int ClassifyAabbToPlane(Plane& _plane, AABB& _aabb);
+	//void BuildFrustum(Frustum& _frustum, float _fov, float _nearPlane, float _farPlane, float _ratio, matrix4& _camPos);
 };
