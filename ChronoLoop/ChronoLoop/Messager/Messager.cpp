4e878fbc88978ad478f2862c84fcab01a1101f58
Raymondckd <rl.lewski0@gmail.com>
2017-02-08 14:08:01 -0500

    More work on the Messager, has a queue and a thread to process messages.

--- ChronoLoop/ChronoLoop/Messager/Messager.cpp
+++ ChronoLoop/ChronoLoop/Messager/Messager.cpp
@@ -1,28 +1,51 @@
-// Messager.cpp : Defines the exported functions for the DLL application.
-//
-
 #include "stdafx.h"
 #include "Messager.h"
-#include "Message.h"
-//#include "..\SoundEngine\SoundEngine.h"
 #include <thread>
 
+template<class... Args>
 Messager::Messager()
 {
-	std::thread thrd (&Messager::Process, this);
+	std::thread thrd(&Messager::Process, this);
 	thrd.detach();
 }
+
 Messager::~Messager()
 {
 
 }
 
-void Messager::SendInMessage()
+template<class... Args>
+void Messager::SendInMessage(Message<Args...> _msg)
+{
+	msgQueue.push_back(_msg);
+}
+//?Need? just call the function needed?
+template<class... Args>
+void Messager::SendOutMessage(Message<Args...> _msg)
 {
 
 }
 
-void Messager::SendOutMessage()
+template<class... Args>
+void Messager::ProcessSound(Message<Args...> _msg)
+{
+	switch (_msg.smType)
+	{
+
+	}
+}
+template<class... Args>
+void Messager::ProcessRender(Message<Args...> _msg)
+{
+
+}
+template<class... Args>
+void Messager::ProcessPhysics(Message<Args...> _msg)
+{
+
+}
+template<class... Args>
+void Messager::ProcessInput(Message<Args...> _msg)
 {
 
 }
@@ -30,10 +53,46 @@ void Messager::SendOutMessage()
 template<class... Args>
 void Messager::ProcessMessage(Message<Args...> _msg)
 {
-
+	switch (_msg.typ)
+	{
+	case messageTypes::SoundEngine:
+	{
+		ProcessSound(_msg);
+	}
+	break;
+	case messageTypes::RenderEngine:
+	{
+		ProcessRender(_msg);
+	}
+	break;
+	case messageTypes::Input:
+	{
+		ProcessInput(_msg);
+	}
+	break;
+	case messageTypes::Physics:
+	{
+		ProcessPhysics(_msg);
+	}
+	break;
+	}
 }
 
+template<class... Args>
 void Messager::Process()
 {
 	//While alive, process messages in the queue
+	while (true)
+	{
+		if (msgQueue.empty())
+			continue;
+
+		Message<Args...> * getMsg = nullptr;
+
+		getMsg = msgQueue.top();
+		ProcessMessage(getMsg);
+		msgQueue.pop();
+		delete getMsg;
+
+	};
 }
--- ChronoLoop/ChronoLoop/Messager/Messager.h
+++ ChronoLoop/ChronoLoop/Messager/Messager.h
@@ -1,34 +1,165 @@
 #pragma once
 #include "..\Sound\SoundEngine.h"
+#include <queue>
+#include <vector>
+#include <cstdarg>
+
+enum messageTypes {
+	NONE = -1,
+	SoundEngine,
+	RenderEngine,
+	Input,
+	Physics
+	
+};
+
+enum soundMessages
+{
+	//SoundEngine
+	INITAILIZE_Audio,
+	SHUTDOWN_Audio,
+	UPDATE_Audio,
+	ADD_Listener,
+	REMOVE_Listener,
+	ADD_Emitter,
+	REMOVE_Emitter,
+	SET_BasePath,
+	ADD_Soundbank,
+	REMOVE_Soundbank
+};
+enum renderMessages
+{
+
+};
+enum inputMessages
+{
+
+};
+enum physicsMessages
+{
+
+};
 
 template<class... Args>
-struct Message;
+struct Message
+{
+	long age;
+	bool isPriority;
+
+	messageTypes typ;
+	union
+	{
+		int num;
+		soundMessages smType;
+		renderMessages rmTypes;
+		inputMessages imTypes;
+		physicsMessages pmTypes;
+	};
+	std::tuple<Args...> need;
+
+
+
+	Message()
+	{
+		age = 0;
+		isPriority = false;
+	}
+	Message(Message& _msg)
+	{
+		age = _msg.age;
+		isPriority = _msg.isPriority;
+		typ = _msg.typ;
+		need = _msg.need;
+	}
+
+	template<class...Args>
+	Message(messageTypes _type, Args&&... _args)
+	{
+		typ = _type;
+		need = make_tuple(_args);
+	}
+
+#pragma region Operator Overrides
+
+	bool operator==(const Message& _msg)
+	{
+		return (typ == _msg.typ) && (num == _msg.num) && (need = _msg.need) && (age == _msg.age) && (isPriority == _msg.isPriority);
+	}
+	bool operator==(Message& _msg)
+	{
+		return (typ == _msg.typ) && (num == _msg.num) && (need = _msg.need) && (age == _msg.age) && (isPriority == _msg.isPriority);
+	}
+
+	//Compares if age is less, determines if its older
+	bool operator<(const Message& _msg)
+	{
+		return (age < _msg.age)
+	}
+	bool operator<(Message& _msg)
+	{
+		return (age < _msg.age)
+	}
+
+	//Compares if age is greater, determines if its younger
+	bool operator>(const Message& _msg)
+	{
+		return (age > _msg.age)
+	}
+	bool operator>(Message& _msg)
+	{
+		return (age > _msg.age)
+	}
+
+#pragma endregion
+
+};
+
+struct CMP {
+
+	template<class... Args>
+	bool compare(const Message<Args...>& _r, const Message<Args...>& _l)
+	{
+		return true;
+	}
+};
+
 
 class Messager
 {
 	static Messager* mMessenger;
 
 private:
+	AudioWrapper audio;
 
-	//AudioWrapper audio;
-
-
+	template<class... Args>
+	static std::priority_queue<Message<Args...>, std::vector<Message<Args...>>, CMP> msgQueue;
 
 
 public:
-	
-
+	template<class... Args>
 	Messager();
 	~Messager();
 
 	Messager* GetMessenger() { return mMessenger; }
 
-	void SendInMessage();
-	void SendOutMessage();
+	template<class... Args>
+	void SendInMessage(Message<Args...> _msg);
+	template<class... Args>
+	void SendOutMessage(Message<Args...> _msg);
 
+	template<class... Args>
 	void Process();
 
 private:
 	template<class... Args>
 	void ProcessMessage(Message<Args...> _msg);
+
+	template<class... Args>
+	void ProcessSound(Message<Args...> _msg);
+	template<class... Args>
+	void ProcessRender(Message<Args...> _msg);
+	template<class... Args>
+	void ProcessPhysics(Message<Args...> _msg);
+	template<class... Args>
+	void ProcessInput(Message<Args...> _msg);
 };
