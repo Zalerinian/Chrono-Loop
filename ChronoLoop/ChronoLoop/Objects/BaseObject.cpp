9228ccecd4c869c3ea18ffb48d9a12a5c179d768
Ryan Bronk <RPBronk@fullsail.edu>
2017-02-09 16:28:51 -0500

    Added Some Physics to the BaseObject & made the Transform cleaner

--- ChronoLoop/ChronoLoop/Objects/BaseObject.cpp
+++ ChronoLoop/ChronoLoop/Objects/BaseObject.cpp
@@ -12,6 +12,14 @@ BaseObject::BaseObject(std::string _name, Transform _transform)
 	name = _name;
 	parent = nullptr;
 	transform = _transform;
+	mass = 0.0f;
+}
+BaseObject::BaseObject(std::string _name, Transform _transform, float _mass)
+{
+	name = _name;
+	parent = nullptr;
+	transform = _transform;
+	mass = _mass;
 }
 BaseObject::~BaseObject()
 {
@@ -46,4 +54,18 @@ BaseObject const* BaseObject::operator=(BaseObject _equals)
 	//if (this->transform != _equals.transform) this->transform = _equals.transform;
 	if (this->components != _equals.components) this->components = _equals.components;
 	return this;
+}
+void BaseObject::CalcAcceleration(vec4f& _force)
+{
+	acc = Physics::CalcAcceleration(_force, mass);
+}
+void BaseObject::CalcVelocity(vec4f& _force, float _dt)
+{
+	CalcAcceleration(_force);
+	vel = Physics::CalcVelocity(vel, acc, _dt);
+}
+void BaseObject::CalcPosition(vec4f& _force, float _dt)
+{
+	CalcVelocity(_force, _dt);
+	pos = Physics::CalcPosition(pos, vel, _dt);
 }
