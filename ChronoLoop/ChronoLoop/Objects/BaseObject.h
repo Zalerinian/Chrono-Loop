622bf8e9c23b01f2bc16f766020856fcd9c32552
Ryan Bronk <RPBronk@fullsail.edu>
2017-02-12 15:52:17 -0500

    Changed variables to their naming conventions

--- ChronoLoop/ChronoLoop/Objects/BaseObject.h
+++ ChronoLoop/ChronoLoop/Objects/BaseObject.h
@@ -9,14 +9,14 @@ using namespace Physics;
 class BaseObject
 {
 private:
-	float mass;
-	std::string name;
-	BaseObject* parent;
-	Transform transform;
-	vec4f pos, vel, acc;
-	unsigned short UniqueID;
-	std::list<BaseObject*> children;
-	std::vector<Component*> components;
+	float m_mass;
+	std::string m_name;
+	BaseObject* m_parent;
+	Transform m_transform;
+	vec4f m_pos, m_vel, m_acc;
+	unsigned short m_UniqueId;
+	std::list<BaseObject*> m_children;
+	std::vector<Component*> m_components;
 	
 public:
 	//**CONSTRUCTORS**//
@@ -27,37 +27,37 @@ public:
 	void Destroy() { delete this; };
 
 	//**GETTERS/SETTERS**//
-	float						GetMass() { return mass; };
-	void						SetMass(float _mass) { mass = _mass; };
+	float						GetMass() { return m_mass; };
+	void						SetMass(float _mass) { m_mass = _mass; };
 
-	std::string					GetName() { return name; };
-	void						SetName(std::string _name) { name = _name; };
+	std::string					GetName() { return m_name; };
+	void						SetName(std::string _name) { m_name = _name; };
 
-	BaseObject*					GetParent() { return parent; };
-	void						SetParent(BaseObject* _parent) { parent = _parent; };
+	BaseObject*					GetParent() { return m_parent; };
+	void						SetParent(BaseObject* _parent) { m_parent = _parent; };
 
-	Transform					GetTransform() { return transform; };
-	void						SetTransform(Transform _transform) { transform = _transform; };
+	Transform					GetTransform() { return m_transform; };
+	void						SetTransform(Transform _transform) { m_transform = _transform; };
 
-	vec4f						GetPosition() { return pos; };
-	vec4f						GetVelocity() { return vel; };
-	vec4f						GetAcceleration() { return acc; };
+	vec4f						GetPosition() { return m_pos; };
+	vec4f						GetVelocity() { return m_vel; };
+	vec4f						GetAcceleration() { return m_acc; };
 
-	unsigned short&				GetUniqueID() { return UniqueID; };
-	void						SetUniqueID(unsigned short _id) { UniqueID = _id; };
+	unsigned short&				GetUniqueID() { return m_UniqueId; };
+	void						SetUniqueID(unsigned short _id) { m_UniqueId = _id; };
 
-	std::list<BaseObject*>		GetChildren() { return children; };
-	void						SetChildren(std::list<BaseObject*> _children) { children = _children; };
+	std::list<BaseObject*>		GetChildren() { return m_children; };
+	void						SetChildren(std::list<BaseObject*> _children) { m_children = _children; };
 
-	std::vector<Component*>		GetComponents() { return components; };
-	void						SetComponents(std::vector<Component*> _components) { components = _components; };
+	std::vector<Component*>		GetComponents() { return m_components; };
+	void						SetComponents(std::vector<Component*> _components) { m_components = _components; };
 	
 
 	//**FUNCTION**//
-	void						AddComponent(Component* _comp) { components.push_back(_comp); };
+	void						AddComponent(Component* _comp) { m_components.push_back(_comp); };
 	//void						RemoveComponent(Component _comp) { components.remove(_comp); };
-	void						AddChild(BaseObject* _obj) { children.push_back(_obj); };
-	void						RemoveChild(BaseObject* _obj) { children.remove(_obj); };
+	void						AddChild(BaseObject* _obj) { m_children.push_back(_obj); };
+	void						RemoveChild(BaseObject* _obj) { m_children.remove(_obj); };
 	void						CalcAcceleration(vec4f& _force);
 	void						CalcVelocity(vec4f& _force, float _dt);
 	void						CalcPosition(vec4f& _force, float _dt);
