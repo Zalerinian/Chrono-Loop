50798a2f6b96f899de72d6f45b57b29f6dfeaae4
Drew Ritter <dritter@razelon.com>
2017-02-13 11:47:59 -0500

    Added empty file in Logs folder to ensure it exits.
    Added Make Build.bat to create a copy of the project in release which has all the resources so we can easily move it to another device for testing.
    
    Signed-off-by: Drew Ritter <dritter@razelon.com>

--- ChronoLoop/ChronoLoop/Common/Logger.cpp
+++ ChronoLoop/ChronoLoop/Common/Logger.cpp
@@ -1,6 +1,7 @@
 #include "Logger.h"
 #include <iostream>
 #include <chrono>
+#include <ctime>
 
 using namespace std;
 
@@ -20,114 +21,123 @@ SystemLogger::~SystemLogger() {
 }
 
 void SystemLogger::PrintTime() {
-	long long seconds =  chrono::system_clock::now().time_since_epoch().count() / 1000 / 1000;
+	// trying to use chrono
+	long long seconds =  chrono::system_clock::now().time_since_epoch().count() / 1000 / 1000 / 1000;
 	long long hour = (seconds / 60 / 60) % 24;
 	long long minutes = (seconds / 60) % 60;
 	long long remaining = seconds % 60;
+
+	// Using ctime
+	time_t now_t = time(0);
+	tm now;
+	localtime_s(&now, &now_t);
+	bool pm = now.tm_hour >= 12;
+	output << "[" << (now.tm_mon + 1) << "/" << now.tm_mday << "/" << (now.tm_year + 1900) << " " <<
+		(now.tm_hour % 12) << ":" << now.tm_min << ":" << now.tm_sec << (pm ? "pm" : "am") << "] ";
 }
 
 SystemLogger &SystemLogger::operator<<(const char i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 	
 SystemLogger &SystemLogger::operator<<(const short i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const int i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const long i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const long long i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const unsigned char i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const unsigned short i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const unsigned int i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const unsigned long i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const unsigned long long i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const char * i) {
 	cout << i;
+	PrintTime();
 	output << i;
-	//PrintTime();
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(const std::string &i) {
 	cout << i;
-	output << i;
 	//PrintTime();
+	output << i;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(ostream &(*pf)(ostream &)) {
 	cout << pf;
-	output << pf;
 	//PrintTime();
+	output << pf;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(ios &(*pf)(ios &)) {
 	cout << pf;
-	output << pf;
 	//PrintTime();
+	output << pf;
 	return *this;
 }
 
 SystemLogger & SystemLogger::operator<<(ios_base &(*pf)(ios_base &)) {
 	cout << pf;
-	output << pf;
 	//PrintTime();
+	output << pf;
 	return *this;
 }
 
