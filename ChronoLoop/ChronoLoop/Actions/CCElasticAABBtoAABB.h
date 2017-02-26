7d2c21ee4aaa3d6edc55b1ddef5d45bbda089036
pburns96 <pburns96@live.com>
2017-02-25 22:40:54 -0500

    kind of working wall collision

--- ChronoLoop/ChronoLoop/Actions/CCElasticAABBtoAABB.h
+++ ChronoLoop/ChronoLoop/Actions/CCElasticAABBtoAABB.h
@@ -11,32 +11,36 @@ struct CCElasticAABBtoAABB : public CodeComponent
 		if (!colliding && _other.mColliderType != Collider::eCOLLIDER_Plane)
 		{
 			colliding = true;
-			SystemLogger::GetLog() << "BOX COLLISION" << std::endl;
+			//SystemLogger::GetLog() << "BOX COLLISION" << std::endl;
 
 			vec4f max = ((CubeCollider*)&_other)->mMax;
 			vec4f min = ((CubeCollider*)&_other)->mMin;
-			vec4f maxt = ((CubeCollider*)&_col)->mMax;
-			vec4f mint = ((CubeCollider*)&_col)->mMin;
 			vec4f otherCenter = (max + min) * 0.5f;
 			vec4f center = _col.GetPos();
-			
+			vec4f V = center - otherCenter;
+
 			vec4f norm;
-			if (center.x <= max.x && center.x >= min.x)
-				norm = { 1,0,0,0 };
-			else if (center.y <= max.y && center.y >= min.y)
-				norm = { 0,1,0,0 };
-			else if (center.z <= max.z && center.z >= min.z)
+			if ((center.x <= max.x && center.x >= min.x) && V.z > 0)
 				norm = { 0,0,1,0 };
+			else if ((center.y <= max.y && center.y >= min.y) && V.y > 0 && V.y > max.y)
+				norm = { 0,1,0,0 };
+			else if ((center.z <= max.z && center.z >= min.z) && V.x > 0)
+				norm = { 1,0,0,0 };
+			else if ((center.x <= max.x && center.x >= min.x) && V.z < 0)
+				norm = { 0,0,-1,0 };
+			else if ((center.y <= max.y && center.y >= min.y) && V.y < 0 && V.y < min.y)
+				norm = { 0,-1,0,0 };
+			else if ((center.z <= max.z && center.z >= min.z) && V.x < 0)
+				norm = { -1,0,0,0 };
 			////////////////////////////////////////////////////Add corner cases
 
-			vec4f V = center - otherCenter;
-			float dist = norm * V;
-			vec4f N = norm * dist;
-			vec4f point = N + otherCenter;
-			vec4f collisionNormal = (center - point).Normalize();
+			//float dist = norm * V;
+			//vec4f N = norm * dist;
+			//vec4f point = N + otherCenter;
+			//vec4f collisionNormal = (center - point);
 			float avgElasticity = (_col.mElasticity + _other.mElasticity) / 2;
-			_col.mVelocity += collisionNormal * (1 + _col.mElasticity);
-
+			//_col.mVelocity += collisionNormal * (1 + avgElasticity);
+			_col.mVelocity += norm * (1 + avgElasticity);
 
 			////SystemLogger::GetLog() << "BOX COLLISION" << std::endl;
 			//
