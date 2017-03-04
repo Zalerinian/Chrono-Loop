a04de2e4cd531b1904bceadb2e2102a9359a7955
Drew Ritter <dritter@razelon.com>
2017-03-04 18:29:09 -0500

    Limited list has been templated, and appears to be working.
    Signed-off-by: Drew Ritter <dritter@razelon.com>

--- ChronoLoop/ChronoLoop/Common/LimitedList.h
+++ ChronoLoop/ChronoLoop/Common/LimitedList.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "Math.h"
+#include "Logger.h"
 
-namespace Epoch
-{
+namespace Epoch {
 
-	class LimitedList
-	{
-		struct Node
-		{
-			vec2f data;
+	template <class T>
+	class LimitedList {
+		template <class T>
+		struct Node {
+			T data;
 			Node *mNext, *mPrev;
-			Node(vec2f _d) : data(_d) {}
+			Node<T>(T _d) : data(_d) {}
 		};
 
-		Node *mHead, *mTail;
+		Node<T> *mHead, *mTail;
 		unsigned int mSize, mLimit = 9;
 
 	public:
@@ -21,10 +21,86 @@ namespace Epoch
 		unsigned int GetSize();
 		unsigned int GetLimit();
 		void SetLimit(unsigned int _size);
-		void AddHead(vec2f _node);
-		vec2f& operator[](unsigned int _index);
+		void AddHead(T _node);
+		T& operator[](unsigned int _index);
 		void Clear();
 		~LimitedList();
 	};
 
+	template <class T>
+	unsigned int LimitedList<T>::GetSize() {
+		return mSize;
+	}
+
+	template <class T>
+	unsigned int LimitedList<T>::GetLimit() {
+		return mLimit;
+	}
+
+	template <class T>
+	bool LimitedList<T>::IsFull() {
+		return mSize == mLimit;
+	}
+
+	template <class T>
+	void LimitedList<T>::SetLimit(unsigned int _size) {
+		if (_size < mLimit) {
+			SystemLogger::GetError() << "[Warning] The requested size (" << _size << ") is smaller than the current size (" << mLimit << "). A memory leak is probable." << std::endl;
+		}
+		if (_size > 100) {
+			SystemLogger::GetError() << "[Warning] The requested size (" << _size << ") is greater than 100. Are you sure LimitedList is the best solution for your problem?" << std::endl;
+		}
+		mLimit = _size;
+	}
+
+	template <class T>
+	void LimitedList<T>::AddHead(T _value) {
+		if (mHead == nullptr) {
+			mHead = new Node<T>(_node);
+			mTail = mHead;
+			mHead->mNext = mHead->mPrev = nullptr;
+			mSize = 1;
+		} else {
+			if (mSize >= mLimit) {
+				Node<T> *tail = mTail->mPrev;
+				delete mTail;
+				mTail = tail;
+				mTail->mNext = nullptr;
+				--mSize;
+			}
+			Node<T>* n = new Node<T>(_node);
+			n->mPrev = nullptr;
+			n->mNext = mHead;
+			mHead->mPrev = n;
+			mHead = n;
+			mSize++;
+		}
+	}
+
+
+	template <class T>
+	T& LimitedList<T>::operator[](unsigned int _index) {
+		Node<T>* n = mHead;
+		for (unsigned int i = 0; i < _index; ++i) {
+			n = mHead->mNext;
+		}
+		return n->data;
+	}
+
+	template <class T>
+	void LimitedList<T>::Clear() {
+		Node<T>* n = mHead;
+		while (n) {
+			Node<T>* self = n;
+			n = n->mNext;
+			delete self;
+		}
+	}
+
+	template <class T>
+	LimitedList<T>::~LimitedList() {
+		Clear();
+	}
+
 }
+
