342a6a3efe933062acdbba87716c47f8a7ba363d
Drew Ritter <dritter@razelon.com>
2017-03-15 10:01:44 -0400

    The pixel shader now uses structs for the light variables instead of just putting variables in places, making it easier to add more lights later.
    Signed-off-by: Drew Ritter <dritter@razelon.com>

--- ChronoLoop/ChronoLoop/Rendering/Shaders/TexturedPixel.hlsl
+++ ChronoLoop/ChronoLoop/Rendering/Shaders/TexturedPixel.hlsl
@@ -10,22 +10,44 @@ SamplerState normalFilter : register(s1);
 texture2D tSpecular : register(t2);
 SamplerState specularFilter : register(s2);
 
-cbuffer DirectionalLight : register(b0)
-{
+struct DLight {
 	float4 dir;
-	float4 dcol;
+	float4 color;
 };
-cbuffer PointLight : register(b1)
-{
-	float4 ppos; 
-	float4 pcol;
+
+struct PLight {
+	float4 pos;
+	float4 color;
 };
-cbuffer SpotLight : register(b2)
+
+struct SLight {
+	float4 dir;
+	float4 pos;
+	float4 color;
+	float  ratio;
+};
+
+cbuffer _DLight : register(b0)
 {
-	float4 cdir;
-	float4 slpos;
-	float4 slcol;
-	float cratio;
+	DLight DirLight;
+	//float4 dir;
+	//float4 dcol;
+};
+
+cbuffer _PLight : register(b1)
+{
+	PLight PointLight;
+	//float4 ppos; 
+	//float4 pcol;
+};
+
+cbuffer _SLight : register(b2)
+{
+	SLight SpotLight;
+	//float4 cdir;
+	//float4 slpos;
+	//float4 slcol;
+	//float cratio;
 };
 
 struct PSI
@@ -44,9 +66,9 @@ float4 main(PSI input) : SV_TARGET
 	//float4 diffuseColor = float4(1, 1, 1, 1);
 	clip(diffuseColor.a - 0.25);
 
-	l1 = ApplyDirectionalLight(dir, input.normal, dcol, diffuseColor);
-	l2 = ApplyPointLight(ppos, input.wpos, input.normal, pcol, diffuseColor);
-	l3 = ApplySpotLight(input.normal, slpos, input.wpos, cdir, cratio, slcol, diffuseColor);
+	l1 = ApplyDirectionalLight(DirLight.dir, input.normal, DirLight.color, diffuseColor);
+	l2 = ApplyPointLight(PointLight.pos, input.wpos, input.normal, PointLight.color, diffuseColor);
+	l3 = ApplySpotLight(input.normal, SpotLight.pos, input.wpos, SpotLight.dir, SpotLight.ratio, SpotLight.color, diffuseColor);
 
 	color = l1 + l2 + l3;
 
