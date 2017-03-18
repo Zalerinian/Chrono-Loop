1ef8cef07e30df792c478934b3c42e4956bced48
Drew Ritter <dritter@razelon.com>
2017-03-18 19:07:56 -0400

    Added Hourglass, along with its gitignore.
    Signed-off-by: Drew Ritter <dritter@razelon.com>

---
+++ Hourglass/Hourglass/Controls/NoFocusButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace LevelEditor
+{
+    public partial class NoFocusButton : Button
+    {
+        public NoFocusButton() : base()
+        {
+            InitializeComponent();
+
+            this.SetStyle(ControlStyles.Selectable, false);
+            this.MinimumSize = new Size(1, 1);
+        }
+
+        protected override bool ShowFocusCues
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
