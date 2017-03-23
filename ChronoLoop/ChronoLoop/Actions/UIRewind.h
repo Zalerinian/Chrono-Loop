f204a556030181be6e401115e30b9222edbe4aaa
Chris Burt <Cwburt@fullsail.edu>
2017-03-23 18:13:31 -0400

    Changed assets

--- ChronoLoop/ChronoLoop/Actions/UIRewind.h
+++ ChronoLoop/ChronoLoop/Actions/UIRewind.h
@@ -3,6 +3,7 @@
 #include "../Input/Controller.h"
 #include "../Input/VRInputManager.h"
 #include "../Objects/BaseObject.h"
+#include "../Common/Math/matrix4.h"
 
 namespace Epoch
 {
@@ -12,7 +13,7 @@ namespace Epoch
 		virtual void Update()
 		{
 			if (VRInputManager::GetInstance().GetController(ControllerType::eControllerType_Primary).CheckGesture() == -1)
-				mObject->GetTransform().SetMatrix(mObject->GetTransform().GetMatrix() * matrix4::CreateYRotation(0.0872665f));
+				mObject->GetTransform().SetMatrix(mObject->GetTransform().GetMatrix().RotateInPlace(vec3f(0,1,0), 0.0872665f));
 		}
 	};
 
