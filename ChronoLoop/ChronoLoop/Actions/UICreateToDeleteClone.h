beb14e7c190ead4a60fb4caeaf85a2c8aae1bb3f
Chris Burt <Cwburt@fullsail.edu>
2017-03-25 16:37:20 -0400

    pulling from pats branch to get things needed to do UICreateToDeleteClone

--- ChronoLoop/ChronoLoop/Actions/UICreateToDeleteClone.h
+++ ChronoLoop/ChronoLoop/Actions/UICreateToDeleteClone.h
@@ -1,14 +1,28 @@
 #pragma once
 #include "CodeComponent.hpp"
+#include "../Objects/MeshComponent.h"
+#include "../Objects/BaseObject.h"
+#include "../Actions/TimeManipulation.h"
+#include "../Core/LevelManager.h"
 
 namespace Epoch
 {
 
 	struct UICreateToDeleteClone : public CodeComponent
 	{
+		bool once = false;
 		virtual void Update()
 		{
-
+			if (LevelManager::GetInstance().GetCurrentLevel()->GetRightTimeManipulator(). /*(GetAsyncKeyState(VK_UP))*/)
+			{
+				((MeshComponent*)mObject->GetComponentIndexed(eCOMPONENT_MESH, 0))->SetVisible(true);
+				once = false;
+			}
+			else if (!once)
+			{
+				once = true;
+				((MeshComponent*)mObject->GetComponentIndexed(eCOMPONENT_MESH, 0))->SetVisible(false);
+			}
 		}
 	};
 
