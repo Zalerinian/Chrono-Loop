aa85e5324c35343dca3a8e10ad6094790c3d0e66
Drew Ritter <dritter@razelon.com>
2017-03-26 21:48:00 -0400

    Raycasting to meshes is working, albeit in a way that seems very backwards.
    Signed-off-by: Drew Ritter <dritter@razelon.com>

---
+++ Hourglass/Hourglass/General/MathExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.DirectX;
+
+namespace Hourglass
+{
+	public static class MathExtension
+	{
+		public static bool SameSign(this float v1, float v2)
+		{
+			return (v1 > 0 && v2 > 0) || (v1 < 0 && v2 < 0);
+		}
+	}
+}
