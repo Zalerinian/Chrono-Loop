6684345a027e10199d43b2ed828b448549647803
Raymondckd <rl.lewski0@gmail.com>
2017-03-29 19:57:17 -0400

    Particles for clones

--- ChronoLoop/ChronoLoop/Core/TimeManager.cpp
+++ ChronoLoop/ChronoLoop/Core/TimeManager.cpp
@@ -11,6 +11,8 @@
 #include "../Common/Breakpoint.h"
 #include "LevelManager.h"
 #include "../Common/Settings.h"
+#include "../Particles/ParticleSystem.h"
+#include "../Messager/Messager.h"
 
 namespace Epoch {
 
@@ -24,7 +26,6 @@ namespace Epoch {
 		CommandConsole::Instance().AddCommand(L"/SNAPCOUNT", ToggleSnapshotCountDisplay);
 
 	}
-
 
 	TimeManager::~TimeManager() {
 		//Level manager will clear delete clones
@@ -265,6 +266,17 @@ namespace Epoch {
 			for (int i = 0; i < mClones.size(); ) {
 				del = false;
 				if (mClones[i]->GetUniqueId() == _id1 || mClones[i]->GetUniqueId() == pair.mOther1 || mClones[i]->GetUniqueId() == pair.mOther2) {
+
+					Particle * p = &Particle::Init();
+					p->SetColors(vec4f(1, 1, 1, 1), vec4f());
+					p->SetLife(300);
+					p->SetSize(.25f, .15f);
+					vec3f EPos = vec3f(mClones[i]->GetTransform().GetPosition()->x, mClones[i]->GetTransform().GetPosition()->y, mClones[i]->GetTransform().GetPosition()->z);
+					ParticleEmitter *emit = new ParticleEmitter(600, 200, 20, EPos);
+					emit->SetParticle(p);
+					emit->SetTexture("../Resources/BasicCircleP.png");
+					ParticleSystem::Instance()->AddEmitter(emit);
+
 					mClones[i]->RemoveAllComponents();
 
 					for (int k = 0; k < Physics::Instance()->mObjects.size(); ++k) {
@@ -317,6 +329,10 @@ namespace Epoch {
 					Pool::Instance()->iAddObject(mClones[i]);
 					mClones.erase(mClones.begin() + i);
 					del = true;
+
+					vec4f temp = EPos;
+					AudioWrapper::GetInstance().MakeEventAtLocation(AK::EVENTS::SFX_BLOP, &temp);
+					emit->FIRE();
 				}
 				if (!del)
 					i++;
