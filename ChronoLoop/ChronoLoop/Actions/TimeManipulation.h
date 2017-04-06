22eb0ee306c2714b68f3b3eae94e43a88c949fb2
pburns96 <PMBurns@fullsail.edu>
2017-04-06 12:57:54 -0400

    Changed Level2.xml control pannels

--- ChronoLoop/ChronoLoop/Actions/TimeManipulation.h
+++ ChronoLoop/ChronoLoop/Actions/TimeManipulation.h
@@ -18,7 +18,7 @@ namespace Epoch
 
 		bool mTexturedApplied = false;
 		bool mPauseTime = false;
-		bool mIsBeingMade, mFireCreationParticles;
+		bool mIsBeingMade, mFireCreationParticles = false;
 		Renderer::TimeManipulationEffectData mEffectData;
 		Interpolator<vec2f> mDesaturationInterpolator;
 		unsigned int HotfixButtonDown = 0;
