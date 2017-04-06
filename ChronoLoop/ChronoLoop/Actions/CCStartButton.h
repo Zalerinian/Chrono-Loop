f91227ee30771f273e633e115deba26fdba358fd
Raymondckd <rl.lewski0@gmail.com>
2017-04-06 10:20:06 -0400

    Sound on hub fixed

--- ChronoLoop/ChronoLoop/Actions/CCStartButton.h
+++ ChronoLoop/ChronoLoop/Actions/CCStartButton.h
@@ -46,6 +46,8 @@ namespace Epoch
 			l = new Listener();
 			mChamberObject->AddComponent(l);
 			AudioWrapper::GetInstance().AddListener(l, "shit");
+
+			((AudioEmitter*)mChamberObject->GetComponentIndexed(ComponentType::eCOMPONENT_AUDIOEMITTER, 2))->CallEvent(Emitter::EventType::ePlay);
 		}
 
 		virtual void OnCollision(Collider& _col1, Collider& _col2, float _time)
@@ -95,11 +97,11 @@ namespace Epoch
 		virtual void Update()
 		{
 
-			if (!AudioToggle)
-			{
-				AudioWrapper::GetInstance().MakeEventAtListener(AK::EVENTS::PLAY_HUB0);
-				AudioToggle = true;
-			}
+			//if (!AudioToggle)
+			//{
+			//	//AudioWrapper::GetInstance().MakeEventAtListener(AK::EVENTS::PLAY_HUB0);
+			//	AudioToggle = true;
+			//}
 
 			if (mBooped)
 			{
@@ -110,7 +112,7 @@ namespace Epoch
 				mExitButtonInterp->Update(TimeManager::Instance()->GetDeltaTime());
 				mExitStandInterp->Update(TimeManager::Instance()->GetDeltaTime());
 				mExitSignInterp->Update(TimeManager::Instance()->GetDeltaTime());
-				
+
 				((ButtonCollider*)mObject->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->SetPos(mObject->GetTransform().GetMatrix().fourth);
 				((ButtonCollider*)mObject->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->mLowerBound.mOffset = mObject->GetTransform().GetMatrix().fourth.y - .2f;
 				((ButtonCollider*)mObject->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->mUpperBound.mOffset = mObject->GetTransform().GetMatrix().fourth.y - .2f;
@@ -119,6 +121,12 @@ namespace Epoch
 				((ButtonCollider*)mExitButton->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->mLowerBound.mOffset = mExitButton->GetTransform().GetMatrix().fourth.y - .2f;
 				((ButtonCollider*)mExitButton->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->mUpperBound.mOffset = mExitButton->GetTransform().GetMatrix().fourth.y - .2f;
 
+				if (!AudioToggle)
+				{
+					((SFXEmitter*)mChamberObject->GetComponentIndexed(ComponentType::eCOMPONENT_AUDIOEMITTER, 0))->CallEvent();
+					((AudioEmitter*)mChamberObject->GetComponentIndexed(ComponentType::eCOMPONENT_AUDIOEMITTER, 1))->CallEvent(Emitter::EventType::ePlay);
+					AudioToggle = true;
+				}
 				bool complete = mPlayerInterp->Update(TimeManager::Instance()->GetDeltaTime());
 				if (complete)
 				{
