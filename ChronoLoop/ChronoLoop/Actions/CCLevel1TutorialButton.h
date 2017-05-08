84e89167aa4a5e49a3a53df6c12c68fd4b68d76b
pburns96 <pburns96@live.com>
2017-05-08 17:00:40 -0400

    lets the player hit the button in the tutorial area more than once

--- ChronoLoop/ChronoLoop/Actions/CCLevel1TutorialButton.h
+++ ChronoLoop/ChronoLoop/Actions/CCLevel1TutorialButton.h
@@ -21,6 +21,7 @@ namespace Epoch
 
 		virtual void Start()
 		{
+			((ButtonCollider*)mObject->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->mPress = true;
 			Level* clevel = LevelManager::GetInstance().GetCurrentLevel();
 			tutButton = clevel->FindObjectWithName("tutButton");
 			tutChamber = clevel->FindObjectWithName("tutChamber");
@@ -39,38 +40,36 @@ namespace Epoch
 		{
 			if (!Settings::GetInstance().GetBool("PauseMenuUp"))
 			{
-				if (!colliding && _other.mColliderType != Collider::eCOLLIDER_Plane && ((Component*)&_other)->GetBaseObject()->GetName().find("Buttonstand"))
-				{
+				if (!colliding && _other.mColliderType != Collider::eCOLLIDER_Plane && ((Component*)&_other)->GetBaseObject()->GetName().find("Buttonstand")) {
 					colliding = true;
 
 					vec3f norm = ((ButtonCollider*)&_col)->mPushNormal;
 					vec3f tForce = norm * (norm * _other.mTotalForce);
 					vec3f vel = norm * (norm * _other.mVelocity);
 					vec3f accel = norm * (norm * _other.mAcceleration);
-					if (tForce * norm < 0 && vel * norm < 0 && accel * norm < 0)
-					{
+					if (tForce * norm < 0 && vel * norm < 0 && accel * norm < 0) {
 						_col.mTotalForce = tForce;
 						_col.mVelocity = vel;
 						_col.mAcceleration = vel / _time;
 
-						if (!interp && Settings::GetInstance().GetInt("tutStep") == 1)//Teleported
-						{
+						doorInterp->SetActive(true);
+						doorInterp->Prepare(0.69f, tutDoor->GetTransform().GetMatrix(), doorEnd, tutDoor->GetTransform().GetMatrix());
+						interp = true;
+
+						if (Settings::GetInstance().GetInt("tutStep") == 1) {
 							Settings::GetInstance().SetUInt("tut1ButtonPress", TimeManager::Instance()->GetCurrentSnapFrame());
 							float t = Settings::GetInstance().GetUInt("tut1ButtonPress");
-							doorInterp->SetActive(true);
-							doorInterp->Prepare(0.69f, tutDoor->GetTransform().GetMatrix(), doorEnd, tutDoor->GetTransform().GetMatrix());
 							chamberInterp->SetActive(true);
 							chamberInterp->Prepare(0.2f, tutChamber->GetTransform().GetMatrix(), chamberEnd, tutChamber->GetTransform().GetMatrix());
-							interp = true;
+						}
 
-							if (tutDoor->GetComponentCount(eCOMPONENT_AUDIOEMITTER) > 0)
-							{
-								if (dynamic_cast<AudioEmitter*>(tutDoor->GetComponentIndexed(eCOMPONENT_AUDIOEMITTER, 0)))
-									((AudioEmitter*)tutDoor->GetComponentIndexed(eCOMPONENT_AUDIOEMITTER, 0))->CallEvent(Emitter::ePlay);
-							}
+						if (tutDoor->GetComponentCount(eCOMPONENT_AUDIOEMITTER) > 0) {
+							if (dynamic_cast<AudioEmitter*>(tutDoor->GetComponentIndexed(eCOMPONENT_AUDIOEMITTER, 0)))
+								((AudioEmitter*)tutDoor->GetComponentIndexed(eCOMPONENT_AUDIOEMITTER, 0))->CallEvent(Emitter::ePlay);
 						}
 					}
-				}
+				} else
+					colliding = false;
 			}
 		}
 
@@ -93,7 +92,8 @@ namespace Epoch
 			{
 				if (doorInterp->Update(TimeManager::Instance()->GetDeltaTime()))
 					interp = false;
-				if(chamberInterp->Update(TimeManager::Instance()->GetDeltaTime()))
+				
+				if(Settings::GetInstance().GetInt("tutStep") == 1 && chamberInterp->Update(TimeManager::Instance()->GetDeltaTime()))
 					Settings::GetInstance().SetInt("tutStep", 2);//Pause Time
 			}
 		}
