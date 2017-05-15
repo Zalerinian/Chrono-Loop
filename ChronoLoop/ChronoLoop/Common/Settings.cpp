098b4f975d277a28b28eae5ab1b0d21afcb32c80
Drew Ritter <dritter@razelon.com>
2017-05-15 17:50:02 -0400

    Fixed a warning with type casting in Level.cpp.
    Settings now uses const char* instead of strings. A custom struct and an FNV hash method is used to index into the maps instead of strings, since their construction is non-trivial.
    
    Signed-off-by: Drew Ritter <dritter@razelon.com>

--- ChronoLoop/ChronoLoop/Common/Settings.cpp
+++ ChronoLoop/ChronoLoop/Common/Settings.cpp
@@ -19,105 +19,105 @@ namespace Epoch {
 		}
 	}
 
-	bool Settings::_Settings::GetBool(std::string _key) {
+	bool Settings::_Settings::GetBool(SettingsKey _key) {
 		mLock.lock();
 		auto val = mBools[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	byte Settings::_Settings::GetByte(std::string _key) {
+	byte Settings::_Settings::GetByte(SettingsKey _key) {
 		mLock.lock();
 		auto val = mBytes[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	unsigned short Settings::_Settings::GetUShort(std::string _key) {
+	unsigned short Settings::_Settings::GetUShort(SettingsKey _key) {
 		mLock.lock();
 		auto val = mUShorts[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	short Settings::_Settings::GetShort(std::string _key) {
+	short Settings::_Settings::GetShort(SettingsKey _key) {
 		mLock.lock();
 		auto val = mShorts[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	float Settings::_Settings::GetFloat(std::string _key) {
+	float Settings::_Settings::GetFloat(SettingsKey _key) {
 		mLock.lock();
 		auto val = mFloats[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	int Settings::_Settings::GetInt(std::string _key) {
+	int Settings::_Settings::GetInt(SettingsKey _key) {
 		mLock.lock();
 		auto val = mInts[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	unsigned int Settings::_Settings::GetUInt(std::string _key) {
+	unsigned int Settings::_Settings::GetUInt(SettingsKey _key) {
 		mLock.lock();
 		auto val = mUInts[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	std::string Settings::_Settings::GetString(std::string _key) {
+	std::string Settings::_Settings::GetString(SettingsKey _key) {
 		mLock.lock();
 		auto val = mStrings[_key];
 		mLock.unlock();
 		return val;
 	}
 
-	void Settings::_Settings::SetBool(std::string _key, bool _v) {
+	void Settings::_Settings::SetBool(SettingsKey _key, bool _v) {
 		mLock.lock();
 		mBools[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetByte(std::string _key, byte _v) {
+	void Settings::_Settings::SetByte(SettingsKey _key, byte _v) {
 		mLock.lock();
 		mBytes[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetUShort(std::string _key, unsigned short _v) {
+	void Settings::_Settings::SetUShort(SettingsKey _key, unsigned short _v) {
 		mLock.lock();
 		mUShorts[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetShort(std::string _key, short _v) {
+	void Settings::_Settings::SetShort(SettingsKey _key, short _v) {
 		mLock.lock();
 		mShorts[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetFloat(std::string _key, float _v) {
+	void Settings::_Settings::SetFloat(SettingsKey _key, float _v) {
 		mLock.lock();
 		mFloats[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetInt(std::string _key, int _v) {
+	void Settings::_Settings::SetInt(SettingsKey _key, int _v) {
 		mLock.lock();
 		mInts[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetUInt(std::string _key, unsigned int _v) {
+	void Settings::_Settings::SetUInt(SettingsKey _key, unsigned int _v) {
 		mLock.lock();
 		mUInts[_key] = _v;
 		mLock.unlock();
 	}
 
-	void Settings::_Settings::SetString(std::string _key, std::string _v) {
+	void Settings::_Settings::SetString(SettingsKey _key, std::string _v) {
 		mLock.lock();
 		mStrings[_key] = _v;
 		mLock.unlock();
