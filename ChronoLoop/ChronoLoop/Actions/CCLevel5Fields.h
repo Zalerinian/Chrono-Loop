55e5413748867b2150a99cb3623d1907a42a1a13
pburns96 <pburns96@live.com>
2017-05-19 21:51:49 -0400

    ryan and chris did a thing to make boxes work

--- ChronoLoop/ChronoLoop/Actions/CCLevel5Fields.h
+++ ChronoLoop/ChronoLoop/Actions/CCLevel5Fields.h
@@ -13,7 +13,7 @@ namespace Epoch
 		matrix4 initialMatrix;
 		float mScaleTipper;
 		bool canBoxShrink,isBoxShrinking;
-		//BoxSnapToControllerAction* leftBS, *rightBS;
+		BoxSnapToControllerAction* leftBS, *rightBS;
 		virtual void Start()
 		{
 			cLevel = LevelManager::GetInstance().GetCurrentLevel();
@@ -23,25 +23,25 @@ namespace Epoch
 			canBoxShrink = false;
 			isBoxShrinking = false;
 
-			//std::vector<Component*> codes1 = LevelManager::GetInstance().GetCurrentLevel()->GetLeftController()->GetComponents(Epoch::ComponentType::eCOMPONENT_CODE);
-			//for (size_t x = 0; x < codes1.size(); ++x)
-			//{
-			//	if (dynamic_cast<BoxSnapToControllerAction*>(codes1[x]))
-			//	{
-			//		leftBS = ((BoxSnapToControllerAction*)codes1[x]);
-			//		break;
-			//	}
-			//}
+			std::vector<Component*> codes1 = LevelManager::GetInstance().GetCurrentLevel()->GetLeftController()->GetComponents(Epoch::ComponentType::eCOMPONENT_CODE);
+			for (size_t x = 0; x < codes1.size(); ++x)
+			{
+				if (dynamic_cast<BoxSnapToControllerAction*>(codes1[x]))
+				{
+					leftBS = ((BoxSnapToControllerAction*)codes1[x]);
+					break;
+				}
+			}
 
-			//codes1 = LevelManager::GetInstance().GetCurrentLevel()->GetRightController()->GetComponents(Epoch::ComponentType::eCOMPONENT_CODE);
-			//for (size_t x = 0; x < codes1.size(); ++x)
-			//{
-			//	if (dynamic_cast<BoxSnapToControllerAction*>(codes1[x]))
-			//	{
-			//		rightBS = ((BoxSnapToControllerAction*)codes1[x]);
-			//		break;
-			//	}
-			//}
+			codes1 = LevelManager::GetInstance().GetCurrentLevel()->GetRightController()->GetComponents(Epoch::ComponentType::eCOMPONENT_CODE);
+			for (size_t x = 0; x < codes1.size(); ++x)
+			{
+				if (dynamic_cast<BoxSnapToControllerAction*>(codes1[x]))
+				{
+					rightBS = ((BoxSnapToControllerAction*)codes1[x]);
+					break;
+				}
+			}
 		}
 
 		virtual void OnTriggerEnter(Collider& _col, Collider& _other)
@@ -64,9 +64,11 @@ namespace Epoch
 					isBoxShrinking = false;
 					canBoxShrink = false;
 					mScaleTipper = 1.0f;
-					//leftBS->mHeld = false;
-					//rightBS->mHeld = false;
+					leftBS->mHeld = false;
+					rightBS->mHeld = false;
 					mBox->GetTransform().SetMatrix(initialMatrix);
+					vec3f pos = vec3f(*mBox->GetTransform().GetPosition());
+					((CubeCollider*)mBox->GetComponentIndexed(eCOMPONENT_COLLIDER, 0))->SetPos(pos);
 				}
 			}
 		}
