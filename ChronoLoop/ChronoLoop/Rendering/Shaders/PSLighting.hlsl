2221e5570fa1310bd6a78dd46f862cff3df56089
Drew Ritter <dritter@razelon.com>
2017-05-22 23:59:25 -0400

    Accidentlly left out the lighting shader, a very important step! The LightCone.obj has also been added, which represents a simple cone volume to simulate spotlights.
    Signed-off-by: Drew Ritter <dritter@razelon.com>

---
+++ ChronoLoop/ChronoLoop/Rendering/Shaders/PSLighting.hlsl
@@ -0,0 +1,53 @@
+#include "Structs.hlsli"
+#include "LightFunctions.hlsli"
+
+struct PSI {
+    float4 position : SV_POSITION;
+    float4 normal : NORMAL0;
+    float4 texCoord : COLOR;
+    float4 wpos : WORLDPOS;
+    float3x3 TBN : TBN;
+    uint IID : CL_IID;
+    uint viewport : SV_ViewportArrayIndex;
+};
+
+texture2D tAlbedo : register(t0);
+texture2D tPosition : register(t1);
+texture2D tNormal : register(t2);
+texture2D tSpecular : register(t3);
+SamplerState fFilter : register(s0);
+
+struct Light
+{
+    float4 color;
+    float4 padding[7];
+};
+
+cbuffer _Eye : register(b0)
+{
+	float3 EyePos;
+}
+
+cbuffer LightProperties : register(b1)
+{
+    Light lights[256];
+}
+
+float4 main(PSI input) : SV_TARGET
+{
+    float3 color = lights[input.IID].color.rgb;
+    float attenuationRadius = lights[input.IID].color.a;
+
+    float screenW = 0, screenH = 0;
+    tAlbedo.GetDimensions(screenW, screenH);
+
+    float2 texCoord = float2(input.position.x / screenW, input.position.y / screenH);
+
+    float4 cAlbedo = tAlbedo.Sample(fFilter, texCoord);
+    float4 pixelNormal = tNormal.Sample(fFilter, texCoord);
+    float3 pixelPosition = tPosition.Sample(fFilter, texCoord);
+
+    float lightRatio = pixelNormal.w < 1 ? 0 : 1;
+
+    return float4((lightRatio * (color * cAlbedo.rgb)).rgb, 0); // Additive blending, so don't add alpha
+}
