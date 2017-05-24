d3afcbc9a2c3aa4bc29964294c9ffca3f4ccd143
Raymondckd <rl.lewski0@gmail.com>
2017-05-24 07:07:16 -0400

    Had ifs backwards

--- ChronoLoop/ChronoLoop/Actions/CCLevel5Button.h
+++ ChronoLoop/ChronoLoop/Actions/CCLevel5Button.h
@@ -111,7 +111,7 @@ namespace Epoch
 					blockMesh->SetVisible(true);
 				}
 
-				if (!blockMesh->IsVisible())
+				if (blockMesh->IsVisible())
 				{
 					std::vector<BaseObject*> l = cLevel->FindAllObjectsByPattern("ExitLight");
 					if (l.size() > 0)
