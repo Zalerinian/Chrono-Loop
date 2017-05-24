fa4fce8bd3c1aac604f14fed7c48c755986f77fc
pburns96 <pburns96@live.com>
2017-05-23 22:35:30 -0400

    fixed gesture issue in rewind UI

--- ChronoLoop/ChronoLoop/Actions/UIRewind.h
+++ ChronoLoop/ChronoLoop/Actions/UIRewind.h
@@ -11,9 +11,11 @@ namespace Epoch
 	struct UIRewind : public CodeComponent
 	{
 		bool once = false;
+		int gesture;
 		virtual void Update()
 		{
-			if (VRInputManager::GetInstance().GetController(ControllerType::eControllerType_Primary).CheckGesture() == -1 /*GetAsyncKeyState(VK_LEFT) & 1*/)
+			gesture = VRInputManager::GetInstance().GetController(ControllerType::eControllerType_Primary).CheckGesture();
+			if (gesture == -1 /*GetAsyncKeyState(VK_LEFT) & 1*/)
 			{
 				if (!once)
 				{
@@ -23,7 +25,7 @@ namespace Epoch
 				mObject->GetTransform().SetMatrix(mObject->GetTransform().GetMatrix().RotateInPlace(vec3f(0, 1, 0), 0.261799f));
 				
 			}
-			else if (VRInputManager::GetInstance().GetController(ControllerType::eControllerType_Primary).CheckGesture() == 1 /*GetAsyncKeyState(VK_RIGHT) & 1*/)
+			else if (gesture == 1 /*GetAsyncKeyState(VK_RIGHT) & 1*/)
 			{
 				if (once)
 				{
